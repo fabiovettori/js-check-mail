56da8f811be71dd4ec25c2754dd22650c80871b8
Fabio Vettori <fabio_vettori@hotmail.it>
2020-10-14 14:55:42 +0200

    General settings

---
+++ sandbox.js
@@ -0,0 +1,4 @@
+// Esercitazione 1: Check lista email
+// Creare un array con un insieme di indirizzi email.
+// Chiedere all'utente il suo indirizzo email e verificare se è nella lista (tramite un ciclo for).
+// Comunicare quindi un messaggio appropriato (sei in lista/non sei in lista)
